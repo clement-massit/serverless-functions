c9b5b47fe3a64df1c800e630394bfdeb782e72f5
clement-massit <clement.massit@gmail.com>
2023-10-30 18:20:25 +0100

    serverless function with some data

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,10 +1,15 @@
 const fetch = require("node-fetch");
+const Papa = require("papaparse"),
+  fs = require("fs");
 
 exports.handler = async function () {
+  let csv = fs.readFileSync("./netlify/functions/data.csv", "utf-8");
+  let csv_json = Papa.parse(csv, { encoding: "utf-8" });
+  console.log(csv_json.data);
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello world!",
+      message: csv_json.data,
     }),
   };
 };
