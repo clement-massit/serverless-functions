67d31ab73c68cf124e636c97c24cf3659deafcb8
clement-massit <clement.massit@gmail.com>
2023-10-30 21:11:06 +0100

    yes

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 exports.handler = async function () {
   //
   const POKE_API =
-    "https://raw.githubusercontent.com/clement-massit/serverless-functions/main/netlify/functions/data.csv";
+    "https://raw.githubusercontent.com/clement-massit/serverless-functions/main/netlify/functions/data.json";
 
   const response = await fetch(POKE_API);
   const data = await response.json();
