28908499db3d467793a659aba9649ef34e207448
clement-massit <clement.massit@gmail.com>
2023-10-30 18:54:53 +0100

    data fixed

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,15 +1,18 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 const Papa = require("papaparse"),
   fs = require("fs");
 
 exports.handler = async function () {
-  let csv = fs.readFileSync("./netlify/functions/data.csv", "utf-8");
-  let csv_json = Papa.parse(csv, { encoding: "utf-8" });
-  console.log(csv_json.data);
+  const beer_csv =
+    "https://raw.githubusercontent.com/clement-massit/serverless-functions/main/dist/data.csv";
+
+  const response = await fetch(beer_csv);
+  const textResp = await response.text();
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: csv_json.data,
+      message: textResp,
     }),
   };
 };
