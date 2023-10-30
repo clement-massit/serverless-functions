94b4d1bf1fd2ddf3e88d9735ca13c49b20823840
clement-massit <clement.massit@gmail.com>
2023-10-30 20:55:37 +0100

    yes

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,24 +1,13 @@
-import fetch from "node-fetch";
-const Papa = require("papaparse"),
-  fs = require("fs");
+const fetch = require("node-fetch");
 
 exports.handler = async function () {
-  // const beer_csv =
-  //   "https://raw.githubusercontent.com/clement-massit/serverless-functions/main/dist/data.csv";
-
-  // const response = await fetch(beer_csv);
-  // const textResp = await response.text();
-  // console.log(textResp);
   const POKE_API = "https://pokeapi.co/api/v2/pokedex/kanto";
 
   const response = await fetch(POKE_API);
   const data = await response.json();
-  console.log(data);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: data,
-    }),
+    body: JSON.stringify(data),
   };
 };
