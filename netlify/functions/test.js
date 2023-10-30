0476dc3e2452c800ec0bffe8693b7de85bd4d6f0
clement-massit <clement.massit@gmail.com>
2023-10-30 18:06:29 +0100

    backend worked

---
+++ netlify/functions/test.js
@@ -0,0 +1,10 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function () {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello world!",
+    }),
+  };
+};
